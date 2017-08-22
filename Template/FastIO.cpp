0268ec5d171d1f4fff1f8ebc69874769c786cfa9
lyjccmm <296006763@qq.com>
2017-08-22 19:18:47 +0800

    Update FastIO.cpp

--- Template/FastIO.cpp
+++ Template/FastIO.cpp
@@ -1,30 +1,38 @@
 #include <cstdio>
 namespace FastIO{
-	const int bN = 1e6;
-	char buf[bN]; int bp;
-	void FastIOinit(){
-		fread(buf, 1, bN, stdin);
+	const int bN = 1;
+	char buf[bN]; int bp, sz;
+	inline bool IOinit() {
+		sz = fread(buf, 1, bN, stdin);
 		bp = 0;
+		return sz != 0;
 	}
-	inline void read(int &x) {
+	inline bool IOcheck() {
+		if (bp == sz) {
+			if(sz == bN) return IOinit();
+			else return false;
+		}
+		return true;
+	}
+	inline bool read(int &x) {
+		if (!IOcheck()) return false;
 		char c; bool sign = false;
 		for (c = buf[bp++]; c < '0' || c > '9'; c = buf[bp++]) {
 			if (c == '-') sign = true;
-			if (bp == bN) FastIOinit();
+			if (!IOcheck()) return false;
 		}
 		for (x = 0; c >= '0' && c <= '9'; c = buf[bp++]) {
 			x = x * 10 + c - '0';
-			if (bp == bN) FastIOinit();
+			if (!IOcheck()) return false;
 		}
 		if (sign) x = -x;
-		if (bp == bN) FastIOinit();
+		return true;
 	}
 }
 using namespace FastIO;
 int main(){
-	FastIOinit();
+	IOinit();
 	int n;
 	read(n);
 	printf("%d\n", n);
 }
-
